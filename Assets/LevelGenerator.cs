6869dcda00ca5a3a6f7dfb875353627b9356a814
Chris Hajduk <chrishajduk84@hotmail.com>
2017-09-30 16:58:18 -0400

    Initial Level Generator Code...Not in a working state

---
+++ Assets/LevelGenerator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        //TODO:Generate Random Seed
+		SimplexNoise.initSimplexNoise();
+        //Generate Corridors
+        Debug.Log(SimplexNoise.noise(0.1,0.1));
+        //Generate Torch placement
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Calculate Torches
+
+	}
+}
